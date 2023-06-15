411caa5556a390fafe1e73d313599fddc4d9581b
Nayana Dharmasiri <nayanad.own@gmail.com>
2023-06-15 19:39:40 +0530

    egress

--- main.tf
+++ main.tf
@@ -28,4 +28,10 @@ resource "aws_security_group" "instance" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
