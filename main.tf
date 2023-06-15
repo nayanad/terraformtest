19ac600986db79e0054583b1c8147f536da534e2
Nayana Dharmasiri <nayanad.own@gmail.com>
2023-06-15 14:30:35 +0530

    SG added to ec2

--- main.tf
+++ main.tf
@@ -6,7 +6,25 @@ resource "aws_instance" "example" {
   ami           =  "ami-024fc608af8f886bc" 
   instance_type = "t2.micro"
 
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  user_data_replace_on_change = true
+
   tags = {
    Name = "NayanaTerraform"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "NayanaTerraform"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
