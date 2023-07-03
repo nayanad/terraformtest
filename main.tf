fa6ccf2d15261d58861f0788d1408d87be459391
Nayana Dharmasiri <nayanad.own@gmail.com>
2023-07-03 16:09:37 +0530

    added output.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "app_server" {
   ami                    = "ami-024fc608af8f886bc"
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.instance.id]
